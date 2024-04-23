9dfbef5a86c1a089b7f1e73dfb78ed37c4da5e2d
yuki035 <yukiyuki1918@icloud.com>
2024-04-24 00:18:56 +0900

    Initial Commit

---
+++ YumemiCodeCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YumemiCodeCheck
+//
+//  Created by 森祐樹 on 2024/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YumemiCodeCheck/YumemiCodeCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumemiCodeCheckApp.swift
+//  YumemiCodeCheck
+//
+//  Created by 森祐樹 on 2024/04/24.
+//
+
+import SwiftUI
+
+@main
+struct YumemiCodeCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
